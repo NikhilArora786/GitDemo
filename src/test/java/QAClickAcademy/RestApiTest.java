10a0533f976b6bbf3f35264f54483e3d084c6e9f
NikhilArora786 <nikarora036@gmail.com>
2020-01-08 21:00:38 +0530

    FirstCommit

---
+++ src/test/java/QAClickAcademy/RestApiTest.java
@@ -0,0 +1,21 @@
+package QAClickAcademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+
+	
+	
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
